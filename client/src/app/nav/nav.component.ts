02cb5682db78f0f5282863d0eb4e394852e66d85
Mike Degteariov <degtyarev@gmail.com>
2024-02-10 22:58:02 -0500

    Lesson 60

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  model: any = {}
 
+  constructor(public accountService: AccountService) {
+   }
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.logn(this.model).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (error: any) => console.log(error)
+    })
+    console.log(this.model);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
