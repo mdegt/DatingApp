d637dd95a19cd8fdd8e79218de7646ef2628ae74
Mike Degteariov <degtyarev@gmail.com>
2024-02-13 14:33:28 -0500

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,22 +11,21 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
    }
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.logn(this.model).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      },
-      error: (error: any) => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
     console.log(this.model);
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,9 +22,12 @@ export class RegisterComponent implements OnInit {
       next: () => {
         this.cancel();
        },
-      error: (error:any) =>  console.log(error)
+      error: error => {
+        this.toastr.error(error.error);
+        console.log(error);
+      }
+
     })
-    console.log(this.model);
   }
 
   cancel() {
