39896c27cd282d807409b0ea0a12cd4a167be658
Mike Degteariov <degtyarev@gmail.com>
2024-02-13 17:35:40 -0500

    Saving

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Errors;
+
+public class ApiException
+{
+    public ApiException(int statusCode, string message, string details)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+    public int StatusCode { get; set; }
+    public String Message { get; set; }
+    public String Details { get; set; }
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
+}
