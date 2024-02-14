8c9fab53f0f6728def76d4d5c737f7aec37bbeca
Mike Degteariov <degtyarev@gmail.com>
2024-02-13 22:08:16 -0500

    Saving

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,4 +1,8 @@
-﻿namespace API.Middleware;
+﻿using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware;
 
 public class ExceptionMiddleware
 {
@@ -11,5 +15,29 @@ public class ExceptionMiddleware
         _next = next;
         _logger = logger;
         _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            
+            var response = _env.IsDevelopment() ? 
+            new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) 
+            : new ApiException(context.Response.StatusCode, "Internal Server Error", null);
+            
+            var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+
+        }
     }
 }
