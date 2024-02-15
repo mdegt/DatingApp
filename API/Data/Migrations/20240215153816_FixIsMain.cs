0f2e74021a3577b34551ffaa8da61c6cfd3c8ec9
Mike Degteariov <degtyarev@gmail.com>
2024-02-15 11:21:01 -0500

    Save

---
+++ API/Data/Migrations/20240215153816_FixIsMain.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIsMain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain1",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "IsMain1");
+        }
+    }
+}
