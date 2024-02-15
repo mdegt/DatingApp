bcdfbb04ee00b0b0b3d881c22c522cb3e29b07b3
Mike Degteariov <degtyarev@gmail.com>
2024-02-15 11:21:31 -0500

    Save

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +41,19 @@ app.MapGet("/wf", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
