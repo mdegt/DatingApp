064bb7773548b6f14f8dbc22a6616fb7130d1526
Mike Degteariov <degtyarev@gmail.com>
2024-02-16 19:54:09 -0500

    Save

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -3,6 +3,7 @@
 public class MemberDto
 {
     public string UserName { get; set; }
+    public string PhotoUrl { get; set; }
     public int Id { get; set; }
      public int Age  { get; set; } 
     public string KnownAs { get; set; }
