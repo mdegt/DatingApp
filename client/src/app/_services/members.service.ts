ca7a03cee262badd4c931a8609805eb6838632d0
Mike Degteariov <degtyarev@gmail.com>
2024-02-28 21:59:02 -0500

    Save

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,23 +12,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if (!userString) {
-      return;
-    }
-    const user = JSON.parse(userString);
-    return {
-      headers: {
-        Authorization: 'Bearer ' + user.token
-      }
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
